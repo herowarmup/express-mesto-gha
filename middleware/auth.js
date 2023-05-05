ce24e2fdccde2c018c321d820e0140dfc30a394a
Artem Morozov <deadtems@yandex.ru>
2023-05-05 05:47:24 +0500

    feat: Закончить подготовку бэкенда

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,25 +1,20 @@
 const jwt = require('jsonwebtoken');
 
 const auth = (req, res, next) => {
-  const { authorization } = req.headers;
+  const token = req.cookies.jwt;
 
-  if (!authorization || !authorization.startsWith('Bearer ')) {
-    req.isAuthenticated = false;
+  if (!token) {
     return res.status(401).send({ message: 'Необходима авторизация' });
   }
 
-  const token = authorization.replace('Bearer ', '');
   let payload;
 
   try {
-    payload = jwt.verify(token, 'secret-key');
+    payload = jwt.verify(token, 'secret-phrase-1234');
   } catch (err) {
-    req.isAuthenticated = false;
     return res.status(401).send({ message: 'Необходима авторизация' });
   }
-
   req.user = payload;
-  req.isAuthenticated = true;
 
   return next();
 };

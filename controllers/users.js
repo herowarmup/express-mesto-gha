1c6fda9a88935c6cdf83e244ba15981fa4a6fa1c
Artem Morozov <deadtems@yandex.ru>
2023-05-06 06:41:21 +0500

    feat: Закончить подготовку бэкенда

--- controllers/users.js
+++ controllers/users.js
@@ -1,11 +1,11 @@
-const jwt = require("jsonwebtoken");
-const bcrypt = require("bcryptjs");
-const User = require("../models/user");
-const { errorHandler } = require("../middleware/errorHandler");
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const User = require('../models/user');
+const { errorHandler } = require('../middleware/errorHandler');
 
 function getUsers(req, res) {
   if (!req.user) {
-    res.status(401).send({ message: "Авторизуйтесь" });
+    res.status(401).send({ message: 'Авторизуйтесь' });
   }
   User.find({})
     .then((users) => res.send({ data: users }))
@@ -18,13 +18,13 @@ async function getUser(req, res) {
   try {
     const user = await User.findById(id);
     if (!user) {
-      res.status(404).send({ message: "Пользователь не найден" });
+      res.status(404).send({ message: 'Пользователь не найден' });
     } else {
       res.send(user);
     }
   } catch (err) {
-    if (err.name === "CastError") {
-      res.status(400).send({ message: "Переданы некорректные данные" });
+    if (err.name === 'CastError') {
+      res.status(400).send({ message: 'Переданы некорректные данные' });
     } else {
       errorHandler(err, res);
     }
@@ -34,12 +34,10 @@ async function getUser(req, res) {
 async function createUser(req, res) {
   bcrypt
     .hash(req.body.password, 10)
-    .then((hash) =>
-      User.create({
-        email: req.body.email,
-        password: hash,
-      })
-    )
+    .then((hash) => User.create({
+      email: req.body.email,
+      password: hash,
+    }))
     .then((user) => {
       const { _id, email } = user;
       res.status(201).json({ _id, email });
@@ -47,7 +45,9 @@ async function createUser(req, res) {
     .catch((err) => {
       res.status(400).send(err);
     });
-  const { name, about, avatar, email, password } = req.body;
+  const {
+    name, about, avatar, email, password,
+  } = req.body;
 
   try {
     const user = await User.create({
@@ -68,35 +68,35 @@ async function updateUser(req, res) {
   const { name, about } = req.body;
 
   if (!req.user) {
-    return res.status(401).json({ message: "Необходима авторизация" });
+    return res.status(401).json({ message: 'Необходима авторизация' });
   }
 
   const userId = req.user._id;
 
   if (!name) {
-    return res.status(400).json({ message: "Имя обязательно для заполнения" });
+    return res.status(400).json({ message: 'Имя обязательно для заполнения' });
   }
 
   if (name.length < 2) {
     return res
       .status(400)
-      .json({ message: "Имя должно быть не менее 2 символов" });
+      .json({ message: 'Имя должно быть не менее 2 символов' });
   }
 
   if (!about) {
     return res
       .status(400)
-      .json({ message: "Описание обязательно для заполнения" });
+      .json({ message: 'Описание обязательно для заполнения' });
   }
 
   try {
     const updatedUser = await User.findByIdAndUpdate(
       userId,
       { name, about },
-      { new: true, runValidators: true }
+      { new: true, runValidators: true },
     );
     if (!updatedUser) {
-      return res.status(404).send({ message: "Пользователь не найден" });
+      return res.status(404).send({ message: 'Пользователь не найден' });
     }
     return res.send(updatedUser);
   } catch (err) {
@@ -110,7 +110,7 @@ async function updateAvatar(req, res) {
   const { avatar } = req.body;
 
   if (!req.user) {
-    return res.status(401).json({ message: "Необходима авторизация" });
+    return res.status(401).json({ message: 'Необходима авторизация' });
   }
 
   const userId = req.user._id;
@@ -119,7 +119,7 @@ async function updateAvatar(req, res) {
     const updatedUser = await User.findByIdAndUpdate(
       userId,
       { avatar },
-      { new: true, runValidators: true }
+      { new: true, runValidators: true },
     );
     res.send(updatedUser);
   } catch (err) {
@@ -131,14 +131,16 @@ async function updateAvatar(req, res) {
 async function login(req, res) {
   const { email, password } = req.body;
 
-  User.findOne({ email })
-    .select(password)
+  User.findUserByCredentials(email, password)
     .then((user) => {
-      res.send({
-        token: jwt.sign({ _id: user._id }, "some-secret-key", {
-          expiresIn: "7d",
-        }),
-      });
+      const token = jwt.sign({ _id: user._id }, 'secret-phrase-1234', { expiresIn: '7d' });
+
+      res.cookie('jwt', token, {
+        maxAge: 3600000,
+        httpOnly: true,
+        sameSite: true,
+      })
+        .send({ token });
     })
     .catch((err) => {
       res.status(401).send({ message: err.message });
@@ -149,7 +151,7 @@ async function getCurrentUser(req, res) {
   console.log(req.user);
   User.findById(req.user._id).then((user) => {
     if (!user) {
-      res.status(404).send({ message: "Пользователь не найден" });
+      res.status(404).send({ message: 'Пользователь не найден' });
     }
     return res
       .send({
